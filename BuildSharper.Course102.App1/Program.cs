0554bf24841c29b335530c13bf5cc4fb4a5a007b
BuildSharper <buildsharper@gmail.com>
2022-08-28 18:35:28 -0500

    Initial commit

---
+++ BuildSharper.Course102.App1/Program.cs
@@ -0,0 +1,55 @@
+﻿////////////////////////////////////////////////////////
+///My Quiz Grades
+////////////////////////////////////////////////////////
+int[] quizGrades = new int[5];
+quizGrades[0] = 87;
+quizGrades[1] = 94;
+quizGrades[2] = 75;
+quizGrades[3] = 88;
+quizGrades[4] = 100;
+
+Console.WriteLine("My Quiz Grades:");
+
+//let's create a variable to total up all of the quiz grades
+int quizTotal = 0;
+
+for (int i = 0; i < 5; i++)
+{
+    //display the quiz grade
+    Console.WriteLine(quizGrades[i] + "%");
+    
+    //add this quiz grade to the quizTotal variable
+    quizTotal += quizGrades[i];
+}
+
+//calculate the average quiz grade
+double quizAverage = quizTotal / (double)5;
+
+Console.WriteLine("Average Quiz Grade: " + quizAverage + "%");
+Console.ReadLine();
+
+////////////////////////////////////////////////////////
+///My Test Grades
+////////////////////////////////////////////////////////
+int[] testGrades = { 95, 79, 87, 89, 98 };
+
+Console.WriteLine("My Test Grades:");
+
+//let's create a variable to total up all of the test grades
+int testTotal = 0;
+
+foreach (int testGrade in testGrades)
+{
+    //display the test grade
+    Console.WriteLine($"{testGrade}%");
+
+    //add the test grade to the testTotal variable
+    testTotal += testGrade;
+}
+
+//calculate the average test grade
+double testAverage = testTotal / (double)5;
+
+Console.WriteLine($"Average Test Grade: {testAverage}%");
+Console.WriteLine("Press any key to continue.");
+Console.ReadLine();
---
+++ BuildSharper.Course102.App2/Program.cs
@@ -0,0 +1,31 @@
+﻿//initialize the 10x10 array for the BattleShip game board, with ships 1, 2, 4, and 5
+int[,] player1Board = new int[10, 10] {
+    { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    { 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+    { 0, 4, 0, 0, 0, 0, 0, 0, 0, 0},
+    { 0, 4, 0, 0, 0, 0, 0, 0, 0, 0},
+    { 0, 4, 0, 0, 0, 2, 0, 0, 0, 0},
+    { 0, 4, 0, 0, 0, 2, 0, 0, 0, 0},
+    { 0, 0, 0, 0, 0, 2, 0, 0, 0, 0},
+    { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    { 0, 0, 0, 5, 5, 5, 5, 5, 0, 0}
+};
+
+//let's manually place boat number 3 in row 1 column 10, row 2 column 10, and row 3 column 10
+player1Board[0, 9] = 3;
+player1Board[1, 9] = 3;
+player1Board[2, 9] = 3;
+
+//now let's display the board, i iterates through the rows
+for (int i = 0; i < 10; i++)
+{
+    //j iterates through the columns
+    for (int j = 0; j < 10; j++)
+    {
+        Console.Write(player1Board[i, j]);
+    }
+
+    //we've reached the end of this row, let's move to the cursor to the next line
+    Console.Write("\r\n");
+}
